0b65bc14bbc958880bdb60777d82cdb920de082d
Varga Máté <matevarga96@gmail.com>
2022-09-26 01:33:20 +0200

    Add 404 error page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import BadgesPage from "./pages/BadgesPage/BadgesPage";
 import HomePage from "./pages/HomePage/HomePage";
 import UsersPage from "./pages/UsersPage/UsersPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 interface AppProps {}
 
@@ -24,6 +25,7 @@ class App extends Component<AppProps, AppState> {
           <Route path="/home" element={<HomePage />} />
           <Route path="/users" element={<UsersPage />} />
           <Route path="/badges" element={<BadgesPage />} />
+            <Route path="*" element={<NotFoundPage />} />
 
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
