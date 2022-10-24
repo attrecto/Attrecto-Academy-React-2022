ff9965f4b3821e29d0c2d3e9a5f0bc53c9d6c426
Varga Máté <matevarga96@gmail.com>
2022-10-24 03:11:47 +0200

    Add table view

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -12,68 +12,110 @@ import { usersService } from "../../services/user.service";
 import classes from "./UsersPage.module.scss";
 
 const UsersPage = () => {
-  const [users, setUsers] = useState<UserModel[]>([]);
-  const navigate = useNavigate();
+    const [users, setUsers] = useState<UserModel[]>([]);
+    const navigate = useNavigate();
 
-  const fetchUsers = useCallback(async () => {
-    setUsers(await usersService.getUsers());
-  }, []);
+    const fetchUsers = useCallback(async () => {
+        setUsers(await usersService.getUsers());
+    }, []);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      setUsers(await usersService.getUsers());
+    useEffect(() => {
+        const fetchUsers = async () => {
+            setUsers(await usersService.getUsers());
+        };
+        fetchUsers();
+    }, []);
+
+    const goToUserPage = () => {
+        navigate("/user");
     };
-    fetchUsers();
-  }, []);
 
-  const goToUserPage = () => {
-    navigate("/user");
-  };
+    const handleDeleteUser = async (id: string) => {
+        await usersService.deleteUser(id);
+        fetchUsers();
+    };
 
-  const handleDeleteUser = async (id: string) => {
-    await usersService.deleteUser(id);
-    fetchUsers();
-  };
+    const [View, setView] = useState(true);
 
-  return (
-    <Page title="Users">
-      <div className="row">
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-          <Button className="w-100 mb-3" onClick={goToUserPage}>
-            Create User
-          </Button>
-        </div>
-      </div>
-      <div className="row">
-        {users.map(({ id, image, name }) => (
-          <div key={id} className="col-12 col-sm-6 col-md-4 col-lg-3 my-1">
-            <Link
-              to={`/user/${id}`}
-              className={classNames("card", classes.UserCard)}
-            >
-              <img
-                src={image}
-                alt={`user #${id}`}
-                className={classNames(classes.UserImage, "card-img-top")}
-              />
-              <div className="card-body">
-                <h5>{name}</h5>
-              </div>
-              <Button
-                className={classes.DeleteIcon}
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleDeleteUser(id.toString());
-                }}
-              >
-                <FontAwesomeIcon icon={faTrash} />
-              </Button>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </Page>
-  );
-};
+    const viewChange = (newView: boolean) => {
+        setView(newView);
+    };
+
+    return (
+            <Page title="Users">
+                <div className="row">
+                    <div className="w-50 col-12 col-sm-6 col-md-4 col-lg-3">
+                        <Button className="mb-3" onClick={goToUserPage}>
+                            Create User
+                        </Button>
+                    </div>
+                    <div className="w-50 col-12 col-sm-6 col-md-4 col-lg-3">
+                        <Button className="float-end" onClick={() => viewChange(true)}>
+                            Card View
+                        </Button>
+                        <Button className="float-end" onClick={() => viewChange(false)}>
+                            Table View
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    {users.map(({id, image, name}) => (
+                        View ?
+                        <div key={id} className="col-12 col-sm-6 col-md-4 col-lg-3 my-1">
+                            <Link
+                                to={`/user/${id}`}
+                                className={classNames("card", classes.UserCard)}
+                            >
+                                <img
+                                    src={image}
+                                    alt={`user #${id}`}
+                                    className={classNames(classes.UserImage, "card-img-top")}
+                                />
+                                <div className="card-body">
+                                    <h5>{name}</h5>
+                                </div>
+                                <Button
+                                    className={classes.DeleteIcon}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handleDeleteUser(id.toString());
+                                    }}
+                                >
+                                    <FontAwesomeIcon icon={faTrash}/>
+                                </Button>
+                            </Link>
+                        </div>
+                            :
+                            <div key={id} className="w-100 col-12 col-sm-6 col-md-4 col-lg-3 my-1">
+                                <Link
+                                    to={`/user/${id}`}
+                                    className={classNames("card", classes.TableUser)}
+                                >
+                                    <div className="TableUserImage">
+                                        <img
+                                            src={image}
+                                            alt={`user #${id}`}
+                                            className={classNames(classes.TableUserImage)}
+                                        />
+                                    </div>
+                                <div className={classNames(classes.TableName)}>
+                                    <h5>{name}</h5>
+                                </div>
+                                <Button
+                                    className={classes.DeleteIcon}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handleDeleteUser(id.toString());
+                                    }}
+                                >
+                                    <FontAwesomeIcon icon={faTrash}/>
+                                </Button>
+                                </Link>
+                            </div>
+                    ))}
+                </div>
+            </Page>
+        );
+    };
 
 export default UsersPage;
